10ea73a371685bd41c77e352fab81a5025fd019a
juliansbaglia18 <47386056+juliansbaglia18@users.noreply.github.com>
2020-05-10 20:15:35 -0300

    Add files via upload

---
+++ TP-02/Entidades/Moto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Moto : Vehiculo
+    {
+        public Moto(EMarca marca, string chasis, ConsoleColor color) 
+            : base(chasis, marca, color)
+        {
+        }
+
+        /// <summary>
+        /// Las motos son chicas
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get 
+            { 
+                return ETamanio.Chico;
+            }
+        }
+
+        /// <summary>
+        /// Muestra los datos de Moto
+        /// </summary>
+        /// <returns>String con el contenido</returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("MOTO");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("TAMAÑO : " + this.Tamanio.ToString());
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
