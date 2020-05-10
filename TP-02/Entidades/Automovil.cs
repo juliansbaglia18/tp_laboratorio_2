607766ff00d709f8e843e8243718fbac950e5b47
juliansbaglia18 <47386056+juliansbaglia18@users.noreply.github.com>
2020-05-10 20:36:23 -0300

    Add files via upload

---
+++ TP-02/Entidades/Automovil.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades
+{
+    public class Automovil : Vehiculo
+    {
+        public enum ETipo
+        { 
+            Monovolumen,
+            Sedan
+        }
+        
+        ETipo tipo;
+
+        /// <summary>
+        /// Por defecto, TIPO será Monovolumen
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Automovil(EMarca marca, string chasis, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+            tipo = ETipo.Monovolumen;
+        }
+
+        /// </summary>
+        /// Por defecto, TIPO será Monovolumen
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        /// <param name="tipo"></param>
+        public Automovil(EMarca marca, string chasis, ConsoleColor color, ETipo tipo) 
+            : this(marca, chasis, color)
+        {
+            this.tipo = tipo;
+        }
+
+        /// <summary>
+        /// Los automoviles son medianos
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Mediano;
+            }
+        }
+
+        /// <summary>
+        /// Muestra los datos de Automovil
+        /// </summary>
+        /// <returns>String con el contenido</returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("AUTOMOVIL");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("TAMAÑO : " + this.Tamanio.ToString());
+            sb.AppendLine("TIPO : " + this.tipo);
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
