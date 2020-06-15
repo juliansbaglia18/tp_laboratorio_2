d26397d58efad409b04192ed24f0bc4c41239dbe
juliansbaglia18 <47386056+juliansbaglia18@users.noreply.github.com>
2020-06-15 05:56:36 -0300

    Add files via upload

---
+++ Clases Instanciables/Profesor.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ClasesAbstractas;
+
+namespace Clases_Instanciables
+{
+    public sealed class Profesor : Universitario
+    {
+        private Queue <Universidad.EClases> clasesDelDia;
+        private static Random random;
+
+        static Profesor()
+        {
+            Profesor.random = new Random();
+        }
+
+        public Profesor()
+            : base()
+        {
+
+        }
+
+        public Profesor(int id, string nombre, string apellido, string dni, ENacionalidad nacionalidad)
+            : base (id, nombre, apellido, dni, nacionalidad)
+        {
+            this.clasesDelDia = new Queue<Universidad.EClases>();
+            this._randomClases();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return this.MostrarDatos();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void _randomClases()
+        {
+            while (this.clasesDelDia.Count < 2)
+            {
+                switch(Profesor.random.Next(1, 4))
+                {
+                    case 1:
+                        this.clasesDelDia.Enqueue(Universidad.EClases.Laboratorio);
+                        break;
+
+                    case 2:
+                        this.clasesDelDia.Enqueue(Universidad.EClases.Legislacion);
+                        break;
+
+                    case 3:
+                        this.clasesDelDia.Enqueue(Universidad.EClases.Programacion);
+                        break;
+
+                    case 4:
+                        this.clasesDelDia.Enqueue(Universidad.EClases.SPD);
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        protected override string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("{0}", base.MostrarDatos());
+            sb.AppendLine(this.ParticiparEnClase());
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        protected override string ParticiparEnClase()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CLASES DEL DIA: ");
+            foreach(Universidad.EClases itemClase in this.clasesDelDia)
+            {
+                sb.AppendLine(itemClase.ToString());
+            }
+
+            return sb.ToString();
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="clase"></param>
+        /// <returns></returns>
+        public static bool operator ==(Profesor i, Universidad.EClases clase)
+        {
+            bool retorno = false;
+
+            foreach(Universidad.EClases item in i.clasesDelDia)
+            {
+                if (item == clase)
+                {
+                    retorno = true;
+                }
+            }
+            return retorno;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="clase"></param>
+        /// <returns></returns>
+        public static bool operator !=(Profesor i, Universidad.EClases clase)
+        {
+            return !(i == clase);
+        }
+
+        public override bool Equals(object obj)
+        {
+            bool retorno = false;
+
+            if(obj is Profesor)
+            {
+                retorno = true;
+            }
+
+            return retorno;
+        }
+    }
+}
---
+++ Clases Instanciables/Universidad.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Excepciones;
+using Archivos;
+using System.Linq;
+using ClasesAbstractas;
+
+namespace Clases_Instanciables
+{
+    public class Universidad
+    {
+        public enum EClases
+        {
+            Programacion,
+            Laboratorio,
+            Legislacion,
+            SPD
+        }
+        private List<Alumno> alumnos;
+        private List<Jornada> jornada;
+        private List<Profesor> profesores;
+
+        public Universidad()
+        {
+            this.alumnos = new List<Alumno>();
+            this.jornada = new List<Jornada>();
+            this.profesores = new List<Profesor>();
+        }
+
+        /// <summary>
+        /// Para operar la lista de alumnos.
+        /// </summary>
+        public List<Alumno> Alumnos
+        {
+            get { return this.alumnos; }
+
+            set { this.alumnos = value; }
+        }
+
+        /// <summary>
+        /// Para operar la lista de profesores.
+        /// </summary>
+        public List<Profesor> Instructores
+        {
+            get { return this.profesores; }
+            set { this.profesores = value; }
+        }
+
+        /// <summary>
+        /// Para operar la lista de jornadas.
+        /// </summary>
+        public List<Jornada> Jornadas
+        {
+            get { return this.jornada; }
+            set { this.jornada = value; }
+        }
+
+        /// <summary>
+        /// Para acceder a la lista de jornadas.
+        /// </summary>
+        /// <param name="i">Indice desde el cual entrar./param>
+        /// <returns>Retorna i si es valido, null si no es.</returns>
+        public Jornada this[int i]
+        {
+            get
+            {
+                if(i >= 0 && i < this.jornada.Count)
+                {
+                    return this.jornada.ElementAt(i);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            set
+            {
+                if(i >= 0 && i < this.Jornadas.Count)
+                {
+                    this.Jornadas[i] = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Guarda los datos de una universidad en un archivo .xlm.
+        /// </summary>
+        /// <param name="uni">Universidad a guardar.</param>
+        /// <returns>True si la universidad es valida.</returns>
+        public static bool Guardar(Universidad uni)
+        {
+            try
+            {
+                string path = System.IO.Directory.GetCurrentDirectory() + @"\ArchivosGuardados";
+                
+                System.IO.Directory.CreateDirectory(path);
+                Xml<Universidad> archivo = new Xml<Universidad>();
+                
+                return archivo.Guardar((path + @"\Universidad.xml"), uni);
+            }
+            catch (Exception e)
+            {
+                throw new ArchivosException(e);
+            }
+        }
+        /// <summary>
+        /// Lee los datos de la universidad guardada en un archivo .xml
+        /// y los retorna en un objeto tipo Universidad.
+        /// </summary>
+        /// <returns>Universidad guardada.</returns>
+        public static Universidad Leer()
+        {
+            try
+            {
+                Universidad uni = new Universidad();
+                
+                Xml<Universidad> archivo = new Xml<Universidad>();
+                archivo.Leer((System.IO.Directory.GetCurrentDirectory() + @"\ArchivosGuardados\Universidad.xml"), out uni);
+                
+                return uni;
+            }
+            catch (Exception e)
+            {
+                throw new ArchivosException(e);
+            }
+        }
+        /// <summary>
+        /// Devuelve los datos de una universidad en una cadena de caracteres.
+        /// </summary>
+        /// <returns>String con datos de la universidad.</returns>
+        private string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+           
+            sb.AppendLine("JORNADA: ");
+            foreach (Jornada itemJornada in this.Jornadas)
+            {
+                sb.AppendFormat("{0}", itemJornada.ToString());
+                sb.AppendLine("<------------------------------------------------->");
+            }
+
+            return sb.ToString();
+        }
+        /// <summary>
+        /// Compara si un alumno esta incluido en una universidad.
+        /// </summary>
+        /// <param name="g">Universidad a comparar.</param>
+        /// <param name="a">Alumno a comparar.</param>
+        /// <returns>Devuelve true si el alumno esta incluido, false si no.</returns>
+        public static bool operator ==(Universidad g, Alumno a)
+        {
+            bool retorno = false;
+
+            foreach (Alumno itemAlumno in g.Alumnos)
+            {
+                if (itemAlumno.Equals(a))
+                {
+                    retorno = true;
+                }
+            }
+            return retorno;
+        }
+        /// <summary>
+        /// Compara si un alumno no esta inluido en una universidad.
+        /// </summary>
+        /// <param name="g">Universidad a comparar.</param>
+        /// <param name="a">Alumno a comparar.</param>
+        /// <returns>Devuelve false si el alumno esta incluido, true si no.</returns>
+        public static bool operator !=(Universidad g, Alumno a)
+        {
+            return !(g == a);
+        }
+        /// <summary>
+        /// Compara si un profesor esta incluido en una universidad.
+        /// </summary>
+        /// <param name="g">Universidad a comparar.</param>
+        /// <param name="i">Profesor a comparar.</param>
+        /// <returns>Devuelve true si el profesor esta incluido, false si no.</returns>
+        public static bool operator ==(Universidad g, Profesor i)
+        {
+            foreach (Profesor itemProfesor in g.Instructores)
+            {
+                if (itemProfesor.Equals(i))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        /// <summary>
+        /// Compara si un profesor no esta incluido en una universidad.
+        /// </summary>
+        /// <param name="g">Universidad a comparar.</param>
+        /// <param name="i">Profesor a comparar.</param>
+        /// <returns>Devuelve false si el profesor esta incluido, true si no.</returns>
+        public static bool operator !=(Universidad g, Profesor i)
+        {
+            return !(g == i);
+        }
+        /// <summary>
+        /// Encuentra al primer profesor capaz de dar la jornada.
+        /// </summary>
+        /// <param name="u">Universidad a comparar.</param>
+        /// <param name="clase">Clase a comparar.</param>
+        /// <returns>Profesor capas de dar la jornada.</returns>
+        public static Profesor operator ==(Universidad u, EClases clase)
+        {
+            foreach (Profesor itemProfesor in u.Instructores)
+            {
+                if (itemProfesor == clase)
+                {
+                    return itemProfesor;
+                }
+            }
+            throw new SinProfesorException();
+        }
+        /// <summary>
+        /// Encuentra el primer profesor incapaz de dar la jornada.
+        /// </summary>
+        /// <param name="i">Universidad a comparar.</param>
+        /// <param name="clase">Clase a comparar.</param>
+        /// <returns>Profesor que no pueda dar la jornada.</returns>
+        public static Profesor operator !=(Universidad i, EClases clase)
+        {
+            foreach (Profesor itemProfesor in i.Instructores)
+            {
+                if (itemProfesor != clase)
+                {
+                    return itemProfesor;
+                }
+            }
+            throw new SinProfesorException();
+        }
+        /// <summary>
+        /// Crea la jornada de la clase indicada y le asigna el profesor
+        /// y los alumnos de ser posible, sino se retorna sin cambios.
+        /// </summary>
+        /// <param name="g">Universidad a comparar.</param>
+        /// <param name="clase">Clase a crear la jornada.</param>
+        /// <returns>Nueva jornada con profesor y alumnos.</returns>
+        public static Universidad operator +(Universidad g, EClases clase)
+        {
+            try
+            {
+                Jornada nuevaJornada = new Jornada(clase, (g == clase));
+                for (int i = 0; i < g.Alumnos.Count; i++)
+                {
+                    if (g.Alumnos[i] == clase)
+                    {
+                        nuevaJornada.Alumnos.Add(g.Alumnos[i]);
+                    }
+                }
+                g.Jornadas.Add(nuevaJornada);
+                return g;
+            }
+            catch (SinProfesorException)
+            {
+                return g;
+            }
+        }
+        /// <summary>
+        /// Agrega un alumnos a la universidad de no ser repetido.
+        /// </summary>
+        /// <param name="u">Universidad a modificar.</param>
+        /// <param name="a">Alumno a agregar.</param>
+        /// <returns>Universidad actualizada.</returns>
+        public static Universidad operator +(Universidad u, Alumno a)
+        {
+            if (u != a)
+            {
+                u.Alumnos.Add(a);
+            }
+            else
+            {
+                throw new AlumnoRepetidoException();
+            }
+            return u;
+        }
+        /// <summary>
+        /// Agrega un profesor a la universidad de no ser repetido
+        /// </summary>
+        /// <param name="u">Universidad a modificar</param>
+        /// <param name="i">Profesor a agregar</param>
+        /// <returns>Universidad actualizada.</returns>
+        public static Universidad operator +(Universidad u, Profesor i)
+        {
+            if (u != i)
+            {
+                u.profesores.Add(i);
+            }
+            return u;
+        }
+        /// <summary>
+        /// Devuelve los datos de la universidad como cadena de caracteres.
+        /// </summary>
+        /// <returns>String con los datos.</returns>
+        public override string ToString()
+        {
+            return this.MostrarDatos();
+        }
+    }
+}
+
