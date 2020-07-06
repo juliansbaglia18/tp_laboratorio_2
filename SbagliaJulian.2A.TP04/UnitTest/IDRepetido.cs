a91e2ee0f2b6164cacaf7ffb9583f7f95e504b59
juliansbaglia18 <47386056+juliansbaglia18@users.noreply.github.com>
2020-07-05 23:27:59 -0300

    Add files via upload

---
+++ SbagliaJulian.2A.TP04/UnitTest/IDRepetido.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class IDRepetido
+    {
+        [TestMethod]
+        [ExpectedException(typeof(TrackingRepetidoException))]
+        public void TestMethod1()
+        {
+            Paquete p1 = new Paquete("adress", "111");
+            Paquete p2 = new Paquete("direccion", "111");
+            Correo c1 = new Correo();
+            c1 += p1;
+            c1 += p2;
+            Assert.Fail();
+        }
+    }
+}
---
+++ SbagliaJulian.2A.TP04/UnitTest/ListaInstanciada.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class ListaInstanciada
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Correo c1 = new Correo();
+            if (c1.Paquetes.Equals(null))
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
